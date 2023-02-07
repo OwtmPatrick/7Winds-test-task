8bc93d933923836db53ff9cdd2fd922545b566cf
Radik Zangirov <owtmpatrick@gmail.com>
2023-02-07 15:18:10 +0500

    fix getting nesting level

--- src/utils/getNestingLevel.ts
+++ src/utils/getNestingLevel.ts
@@ -1,6 +1,10 @@
 import { Row } from '../types';
 
 export default function getNestingLevel(r: Array<Row>, lvl = 0): number {
+  if (!r.length) {
+    return 1;
+  }
+
   const nestedLvls = r.map((item) => {
     if (!item.child.length) {
       return lvl + 1;
